84854e6e25aa043329973d4442a9a1d9c2334896
Harpreet Singh <singhharpreet547@gmail.com>
2020-08-23 14:53:38 +0530

    Basic 3 hit combo manager

---
+++ Assets/Project/Scripts/ComboManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComboManager : MonoBehaviour
+{
+    public static ComboManager instance;
+    public bool canReceiveInput;
+    public bool inputReceived;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canReceiveInput = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("LightAttack"))
+        {
+            if (canReceiveInput)
+            {
+                inputReceived = true;
+                canReceiveInput = false;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+
+    public void attack()
+    {
+        if(Input.GetButtonDown("LightAttack"))
+        {
+            if(canReceiveInput)
+            {
+                inputReceived = true;
+                canReceiveInput = false;
+            } else
+            {
+                return;
+            }
+        }
+    }
+
+    public void inputManager()
+    {
+        if(!canReceiveInput)
+        {
+            canReceiveInput = true;
+        } else
+        {
+            canReceiveInput = false;
+        }
+    }
+}
